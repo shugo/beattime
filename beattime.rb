45c98005cdae3ccd5a531e0c0256541283830aed
Shugo Maeda <shugo@ruby-lang.org>
2015-10-28 17:53:40 +0900

    Refined.

--- beattime.rb
+++ beattime.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 
-class Time
-  def to_beat
-    1000 * ((to_r + 60 * 60) % (24 * 60 * 60)) / (24 * 60 * 60)
+module BeatTime
+  refine Time do
+    def to_beat
+      1000 * ((to_r + 60 * 60) % (24 * 60 * 60)) / (24 * 60 * 60)
+    end
   end
 
-  def self.parse_beat(s)
-    Time.at(s.delete("@").to_r / 1000 * (24 * 60 * 60) - 60 * 60)
+  refine Time.singleton_class do
+    def parse_beat(s)
+      Time.at(s.delete("@").to_r / 1000 * (24 * 60 * 60) - 60 * 60)
+    end
   end
 end
 
 if __FILE__ == $0
+  using BeatTime
+
   if ARGV.empty?
     printf("@%2.2f\n", Time.now.to_beat)
   else
